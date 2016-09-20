f069c294a195a91843a046f11b8cfcb6eb337f36
harsh_000 <harsh_000@USER>
2016-09-20 23:41:19 +0530

    Add project files.

---
+++ Com.Site/App/components/home/home.js
@@ -0,0 +1,4 @@
+﻿/// <reference path="c:\users\harsh_000\documents\visual studio 2015\Projects\Com.Site\Com.Site\scripts/angular.min.js" />
+angular.module("app.controllers", []).controller("homeController", function ($scope) {
+    $scope.Message = "Harshal";
+});
---
+++ Com.Site/App/routes.js
@@ -0,0 +1,16 @@
+﻿/// <reference path="angular.min.js" />
+angular.module('app.route', ['ngRoute']).config(function ($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl: "App/components/home/home.html"
+    })
+    .when("/red", {
+        templateUrl: "red.htm"
+    })
+    .when("/green", {
+        templateUrl: "green.htm"
+    })
+    .when("/blue", {
+        templateUrl: "blue.htm"
+    });
+});
